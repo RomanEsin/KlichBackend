538cc8df7785a8bda3f3bedb3c54f0527219d314
Roman Esin <esinromanswift@gmail.com>
2021-05-22 20:10:42 +1000

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def login(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if found_user is None:
         raise HTTPException(detail="Invalid Username or Password", status_code=400)
 
-    if found_user.password != user.password and found_user.user_type == user.user_type:
+    if found_user.password != user.password:
         raise HTTPException(detail="Invalid Username or Password", status_code=400)
 
     token = db.query(models.UserToken).filter(models.UserToken.user_id == found_user.id).first()
